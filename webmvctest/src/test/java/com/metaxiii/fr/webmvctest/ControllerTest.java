8cb733d5a6ec222c34f1351177981e2ae5b5b7a3
metaxiii <lehchibi.gael@pm.me>
2022-11-23 13:47:39 +0100

    fix code smells

--- webmvctest/src/test/java/com/metaxiii/fr/webmvctest/ControllerTest.java
+++ webmvctest/src/test/java/com/metaxiii/fr/webmvctest/ControllerTest.java
@@ -2,6 +2,7 @@ package com.metaxiii.fr.webmvctest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -10,6 +11,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(Controller.class)
+@AutoConfigureMockMvc
 class ControllerTest {
     @Autowired
     private MockMvc mockMvc;
