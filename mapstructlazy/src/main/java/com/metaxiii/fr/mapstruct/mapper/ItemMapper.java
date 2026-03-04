2297513aba172b45155677adbe5a12fc6d118005
metaxiii <16118999+metaXIII@users.noreply.github.com>
2026-03-04 11:34:41 +0100

    feat: upgrade java 25

--- mapstructlazy/src/main/java/com/metaxiii/fr/mapstruct/mapper/ItemMapper.java
+++ mapstructlazy/src/main/java/com/metaxiii/fr/mapstruct/mapper/ItemMapper.java
@@ -2,15 +2,11 @@ package com.metaxiii.fr.mapstruct.mapper;
 
 import com.metaxiii.fr.mapstruct.entity.ItemEntity;
 import com.metaxiii.fr.mapstruct.model.Item;
-import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
 
-@Mapper(
-  componentModel = "spring",
-  unmappedTargetPolicy = ReportingPolicy.IGNORE,
-  injectionStrategy = InjectionStrategy.CONSTRUCTOR
-)
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ItemMapper {
   Item toDomain(ItemEntity entity);
 }
